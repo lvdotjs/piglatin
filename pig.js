cbd8ec5c87724918610c1cac0d7ecc97fc5e09c0
Jeremy Greer <jex.grizzle@gmail.com>
2020-03-04 08:18:11 -0800

    Refactor pig latin exercise

--- pig.js
+++ pig.js
@@ -3,31 +3,14 @@ const pigIt = (str) => {
     throw new Error('that is not a string, dude')
   }
 
-  let newString = ''
-
   const words = str.split(/\s+/g)
 
-
-  words.forEach((word, index) => {
+  return words.reduce((accumulator, word) => {
     const firstLetter = word[0]
     const restOfIt = word.slice(1)
-
-    // If this is a _real_ word...
-    const isRealWord = /\w/.test(word)
-    if (isRealWord) {
-      newString += `${restOfIt}${firstLetter}`
-      newString += 'ay'
-    } else {
-      newString += `${word}`
-    }
-
-    // If NOT the last item, add some space.
-    const isLastItem = index === words.length - 1
-    if (!isLastItem) {
-      newString += ' '
-    }
-  })
-
-
-  return newString
+    return /\w/.test(word)
+      ? [...accumulator, `${restOfIt}${firstLetter}ay`]
+      : [...accumulator, word]
+  }, [])
+    .join(' ')
 }
